bfdf120a473ea2aece6ec74749f9d0f2fc84283d
German Lobo <germanlobo76@gmail.com>
2024-08-21 16:40:03 -0400

    Commit de actualizacion antes de subir a Render

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,56 @@
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ prelude::*, BufReader };
+use std::fs;
+
 fn main() {
+    let listener = TcpListener::bind("0.0.0.0:10000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+            handle_connection(stream);
+        }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+	.lines()
+	.map(|result| result.unwrap())
+	.take_while(|line| !line.is_empty())
+	.collect();
+
+    // Imprimiendo HTTP_REQUEST para debugging
+    println!("Contenido de HTTP_REQUEST: {:?}", http_request);
+    let (status_line, filename) =
+        if http_request.len() == 0 {
+            run_multithreads();
+	        ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            let binding = http_request[0].clone();
+            let parts: Vec<&str> = binding
+                .split(" ")
+                .collect();
+
+            if parts[1] == "/" || parts[1] == "/?" {
+                run_multithreads();
+                ("HTTP/1.1 200 OK", "index.html")
+            } else {
+                ("HTTP/1.1 404 NOT FOUND", "404.xhtml")
+            }
+        };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{}\r\nContent length: {}\r\n\r\n{}",
+		       status_line,
+		       length,
+		       contents);
+    
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn run_multithreads() {
     multithreaded_rust_news_reader::clean_file("index2.html".to_string());
     multithreaded_rust_news_reader::append_to_file(
         "index2.html".to_string(),
